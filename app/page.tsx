c92ce87506dab2abd2a2d59b008aab15e034bb0a
Valentin Marta <valenmarta17@gmail.com>
2025-06-23 21:16:05 -0300

    prueba de cambio

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import { GithubIcon } from "@/components/icons";
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <h1>Hola mundo</h1>
+      <h1>Hola mundo probando cambio</h1>
     </section>
   );
 }
