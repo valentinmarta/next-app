8c86df54c78792fbc47fc63667e9dffd0fe18017
Valentin Marta <valenmarta17@gmail.com>
2025-06-23 21:02:48 -0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import { Link } from "@heroui/link";
+import { Snippet } from "@heroui/snippet";
+import { Code } from "@heroui/code";
+import { button as buttonStyles } from "@heroui/theme";
+
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+import { GithubIcon } from "@/components/icons";
+
+export default function Home() {
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <h1>hola mundo</h1>
+    </section>
+  );
+}
